c7f5c448f71f9bc6e2ac00e5604da0f8d54748d9
Janakiraman Ganesan <jana.ganesan@tradingtechnologies.com>
2019-04-19 13:50:37 +0900

    Revert "Add Policy Type field."
    This reverts commit 369e5b3279342378c070cf1ac6fb86a6c727fff1.

--- lic/admin.py
+++ lic/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
-from .forms import PolicyTypeForm
 
 # Register your models here.
 
-from .models import Client, Policy, Due, Reminder, PolicyType
-
-class MyAdmin(admin.ModelAdmin):
-    form = PolicyTypeForm
-
+from .models import Client, Policy, Due, Reminder
 
 admin.site.register(Client)
 admin.site.register(Policy)
 admin.site.register(Due)
 admin.site.register(Reminder)
-admin.site.register(PolicyType, MyAdmin)
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='lic/')),
     path('lic/', include('lic.urls')),
     path('admin/', admin.site.urls),
 ]
