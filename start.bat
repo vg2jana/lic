5ff9847c09f9dc7f900d1585576af1d073fb9c50
chandru <mahanutpam@gmail.com>
2019-05-11 15:28:20 +0530

    Proper Django version added on startup script

--- start.bat
+++ start.bat
@@ -9,7 +9,7 @@ python-3.6.5.exe  InstallAllUsers=1 Include_pip=1 PrependPath=1 Include_test=1 I
 
 timeout 3 > nul
 
-set /P inputuser=Press any key to once the python installation complete: 
+set /P inputuser=Press any key once the python installation complete: 
 
 python -V 
 
@@ -21,27 +21,26 @@ if not %errorlevel% equ 0 (
    goto endlocal
 )
 
-pip install Django
-pip install django-common-helpers
-pip install django-cron
-pip install django-tables2
-pip install python-dateutil
-pip install pytz
-pip install quickfix
-pip install requests
-pip install setuptools
-pip install simplejson
-pip install six
-pip install urllib3
-pip install webcolors
-pip install websocket-client
-pip install wheel
-pip install ws4py
+pip install Django==2.1
+pip install django-common-helpers==0.9.1
+pip install django-cron==0.5.0
+pip install django-tables2==1.21.2
+pip install python-dateutil==2.7.3
+pip install pytz==2018.4
+pip install requests==2.19.1
+pip install setuptools==39.0.1
+pip install simplejson==3.16.0
+pip install six==1.11.0
+pip install urllib3==1.23
+pip install webcolors==1.8.1
+pip install websocket-client==0.46.0
+pip install wheel==0.33.1
+pip install ws4py==0.5.1
 
-goto endlocal
 
-python manage.py makemigrations lic
+python manage.py makemigrations insurance
 python manage.py migrate
+python manage.py createsuperuser
 
 :while
 
